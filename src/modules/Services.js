424790d14ed0d29177ac1489abaf5ad83dd84672
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-13 23:04:53 +0200

    created component for services, added styles

--- src/modules/Services.js
+++ src/modules/Services.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <section className="services">
+      <div className="services-container">
+        <div className="service">
+          <img
+            src="./images/icon-access-anywhere.svg"
+            alt="anywhere"
+            className="service-icon"
+          />
+          <h3 className="service-info">Access your files, anywhere</h3>
+          <p className="service-text">
+            The ability to use a smartphone, tablet, or computer to access your
+            account means your files follow you everywhere.
+          </p>
+        </div>
+        <div className="service">
+          <img
+            src="./images/icon-security.svg"
+            alt="security"
+            className="service-icon"
+          />
+          <h3 className="service-info">Security you can trust</h3>
+          <p className="service-text">
+            2-factor authentication and user-controlled encryption are just a
+            couple of the security features we allow to help secure your files.
+          </p>
+        </div>
+        <div className="service">
+          <img
+            src="./images/icon-collaboration.svg"
+            alt="collab"
+            className="service-icon"
+          />
+          <h3 className="service-info">Real-time collaboration</h3>
+          <p className="service-text">
+            Securely share files and folders with friends, family and colleagues
+            for live collaboration. No email attachments required.
+          </p>
+        </div>
+        <div className="service">
+          <img
+            src="./images/icon-any-file.svg"
+            alt="any file"
+            className="service-icon"
+          />
+          <h3 className="service-info">Store any type of file</h3>
+          <p className="service-text">
+            {" "}
+            Whether you're sharing holidays photos or work documents, Fylo has
+            you covered allowing for all file types to be securely stored and
+            shared.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
