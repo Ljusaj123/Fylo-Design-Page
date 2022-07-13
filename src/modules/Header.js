439d9bbfec12cb2ba2a900d1e7ef62e134fb052f
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-13 21:24:50 +0200

    created component for header, styles for header

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo-container">
+          <img src="./images/logo.svg" alt="aaaa" id="logo" />
+        </div>
+        <Navbar />
+        <div className="intro-container">
+          <img
+            src="./images/illustration-intro.png"
+            alt=""
+            className="intro-pic"
+          />
+          <h2 className="intro-title">
+            All your files in one secure location, accessible anywhere.
+          </h2>
+          <p className="intro-text">
+            Fylo stores all your most important files in one secure location.
+            Access them wherever you need, share and collaborate with friends
+            family, and co-workers.
+          </p>
+          <button className="button">Get Started</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
