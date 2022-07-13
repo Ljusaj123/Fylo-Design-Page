80e00d25f0d2f3a506a9d604d9fd6686dec3b5bf
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-13 18:46:53 +0200

    created components and modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Features from "./modules/Features";
+import Services from "./modules/Services";
+import SignIn from "./modules/SignIn";
+import Testimonials from "./modules/Testimonials";
+import Footer from "./modules/Footer";
+import Header from "./modules/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Services />
+      <Features />
+      <Testimonials />
+      <SignIn />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
