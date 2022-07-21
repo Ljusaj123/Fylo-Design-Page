cc60ae0c232e57ab0c38a03dff1a8aa0757a85e0
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-21 16:58:38 +0200

    jsx for sign in module

--- src/modules/SignIn.js
+++ src/modules/SignIn.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <div className="sign-in">
+      <div className="sign-in-container">
+        <div className="text-container">
+          <h2>Get early access today</h2>
+          <p>
+            It only takes a minute to sign up and our free starter tier is
+            extremely generous. If you have any questions, our support team
+            would be happy to help you.
+          </p>
+        </div>
+        <div className="input-button-container">
+          <input type="email" placeholder="email@example.com" />
+          <button className="button">Get started For Free</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
